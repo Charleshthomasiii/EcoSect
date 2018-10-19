220bf7a73b3eaa53bd27359cf48db91286c26a82
Charleshthomasiii <cht327@nyu.edu>
2018-10-19 16:56:58 -0400

    Newest commit moving predator

--- sketch.js
+++ sketch.js
@@ -1,22 +1,21 @@
-var creatures = [];
-var predators = [];
+var creatures = []; //array that holds creatures
+var predators = []; //array that holds predators
 function preload(){
   dirt = loadImage("images/dirt.jpg");
   bush= loadImage("images/bush.png")
 }
 function setup() {
   createCanvas(1300,600,);
-  for (var i = 0; i < 40; i++) {
+  for (var i = 0; i < 80; i++) {
     creatures.push( new preyCreature(random(width), random(height)));
   }
-  // creatures.push(new predatorCreature(200,200));
   predators.push(new predatorCreature(500,500));
   noiseDetail(24);
 }
 
 function draw() {
   // tint(255, 127);
-  image(dirt,0,0,);
+  image(dirt,0,0,);//dirt background
   image(dirt,dirt.width,0)
   image(bush,0,0,90,70);
   for (var i = 0; i < creatures.length; i++) {
@@ -26,6 +25,7 @@ function draw() {
       creatures[i].attract( creatures[j] );
       creatures[i].repulse( creatures[j] );
     }
+
     // for (var l )
 
     // display the creature
@@ -92,13 +92,11 @@ class preyCreature {
     }
 
     // draw the creature
-    fill(255);
-    noStroke();
+   
     this.rotateCreature();
-    // draw the 'attraction zone' for the creature
-     // ellipse(this.x, this.y, this.repulsionZoneSize, this.repulsionZoneSize);
-     this.dx=0;
-     this.dy=0;
+    // ellipse(this.x, this.y, this.repulsionZoneSize, this.repulsionZoneSize);
+    this.dx=0;
+    this.dy=0;
   }
   rotateCreatureMouse(){
     push();
@@ -169,7 +167,6 @@ class preyCreature {
       rect(this.size/7, -this.size/7, this.size/20, -this.size/6);
     }
 
-    fill(229, 125, 34);
     // ellipse(50,50,30,30);
     pop();
   }
@@ -186,7 +183,6 @@ class preyCreature {
       // move 5% of the way to the new creature
       this.dx += changeX * this.attractionStrength;
       this.dy += changeY * this.attractionStrength;
-      this.neighbors++;
     }
 
   }
@@ -224,13 +220,13 @@ class predatorCreature {
     this.repulsed=0;
     this.noiseOffsetX = random(0,1000);
     this.noiseOffsetY = random(0,1000);
-    this.attractionZoneSize = 1000;
+    this.attractionZoneSize = 2000;
     this.preyDecision=[];
     this.index;
     //pick the nearest prey, and chase after it
 
     this.eatZoneSize=this.size*2;
-    this.eatStrength=0.0;
+    // this.eatStrength=0.0;
     this.attractionStrength=0.01;
     this.repulseTime=60;
     this.dx=0;
@@ -244,8 +240,8 @@ class predatorCreature {
   }
   moveAndDisplay() {
     // if (this.repulsed==0) {
-    //   this.dx += map( noise(this.noiseOffsetX), 0, 1, -.5, .5);
-    //   this.dy += map( noise(this.noiseOffsetY), 0, 1, -.5, .5);
+      this.dx += map( noise(this.noiseOffsetX), 0, 1, -1, 1);
+      this.dy += map( noise(this.noiseOffsetY), 0, 1, -1, 1);
     // }
     var min=50000;
     var chosen;
