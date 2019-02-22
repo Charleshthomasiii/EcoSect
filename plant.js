b8acb49119e12d2040318af210901997c1b195d0
Charleshthomasiii <cht327@nyu.edu>
2019-02-22 16:52:36 +0100

    Final Formatting Changes

--- plant.js
+++ plant.js
@@ -1,37 +1,38 @@
-class plant{
-  constructor(x,y) {
-    this.x = x;
-    this.y = y;
-    this.repulsed=0;
-    this.attractionZoneSize = 400;
-    this.health = 50;
-    this.dead = 0;
-    this.deadCounter=90;
-    this.repulsionZoneSize=this.size*2;
-    this.repulsionStrength=0.02;
-    this.attractionStrength=0.1;
-    this.size=50;
-  }
-  healthFunc() {
-    this.health-=0.1;
-    if (this.health<=0){
-      this.deathProcess();
+class plant {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.repulsed = 0;
+        this.attractionZoneSize = 400;
+        this.health = 50;
+        this.dead = 0;
+        this.deadCounter = 90;
+        this.repulsionZoneSize = this.size * 2;
+        this.repulsionStrength = 0.02;
+        this.attractionStrength = 0.1;
+        this.size = 50;
     }
-  }
-  deathProcess(){
-    this.dead=1;
-    this.deadCounter=90;    
-  }
-  moveAndDisplay() {
-    imageMode(CENTER);
-    image(bush,this.x,this.y,this.size,this.size*.95);
-    if (this.dead ==1){
-      this.deadCounter-=1;
-    }
-    else{
-      this.healthFunc();
-    }
-    imageMode(CORNER)
-  }
 
+    healthFunc() {
+        this.health -= 0.1;
+        if (this.health <= 0) {
+            this.deathProcess();
+        }
+    }
+
+    deathProcess() {
+        this.dead = 1;
+        this.deadCounter = 90;
+    }
+    
+    moveAndDisplay() {
+        imageMode(CENTER);
+        image(bush, this.x, this.y, this.size, this.size * .95);
+        if (this.dead == 1) {
+            this.deadCounter -= 1;
+        } else {
+            this.healthFunc();
+        }
+        imageMode(CORNER)
+    }
 }
